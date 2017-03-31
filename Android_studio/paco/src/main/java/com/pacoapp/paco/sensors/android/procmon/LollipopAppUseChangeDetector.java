2f8b99609edee3aa619ff812edf6cff830a065d9
JodliDev <jodlidev@gmail.com>
2017-03-31 12:02:17 +0200

    detectNewApps() and detectClosedApps() were both iterating the same events-List, which means two identical loops for each App-Event * So I removed them and put them in a single loop * Also the lists newApps and closedApps had no use so I removed them too

--- Android_studio/paco/src/main/java/com/pacoapp/paco/sensors/android/procmon/LollipopAppUseChangeDetector.java
+++ Android_studio/paco/src/main/java/com/pacoapp/paco/sensors/android/procmon/LollipopAppUseChangeDetector.java
@@ -32,43 +32,25 @@ public class LollipopAppUseChangeDetector {
     if (events.size() == 0) {
       return;
     }
-    List<AppUsageEvent> newApps = detectNewApps(events);
-    List<AppUsageEvent> closedApps = detectClosedApps(events);
-  }
 
-  private List<AppUsageEvent> detectClosedApps(List<AppUsageEvent> events) {
-    List<AppUsageEvent> results = Lists.newArrayList();
     Lists.reverse(events);
     for (AppUsageEvent appUsageEvent : events) {
       if (appUsageEvent.getType() == AppUsageEvent.MOVE_TO_BACKGROUND_EVENT) {
-
         AppUsageEvent lastClosedRecord = lastClosedMap.get(appUsageEvent.getAppIdentifier());
         if (lastClosedRecord == null || lastClosedRecord.getTimestamp() < appUsageEvent.getTimestamp()) {
           lastClosedMap.put(appUsageEvent.getAppIdentifier(), appUsageEvent);
-          results.add(appUsageEvent);
           listener.appClosed(appUsageEvent, isAppOfInterestForClosing(appUsageEvent));
         }
       }
-    }
-    return results;
-  }
-
-  private List<AppUsageEvent> detectNewApps(List<AppUsageEvent> events) {
-    List<AppUsageEvent> results = Lists.newArrayList();
-    Lists.reverse(events);
-    for (AppUsageEvent appUsageEvent : events) {
-      if (appUsageEvent.getType() == AppUsageEvent.MOVE_TO_FOREGROUND_EVENT) {
-
+      else if (appUsageEvent.getType() == AppUsageEvent.MOVE_TO_FOREGROUND_EVENT) {
         AppUsageEvent lastOpenedRecord = lastOpenedMap.get(appUsageEvent.getAppIdentifier());
         if (lastOpenedRecord == null || lastOpenedRecord.getTimestamp() < appUsageEvent.getTimestamp()) {
           lastOpenedMap.put(appUsageEvent.getAppIdentifier(), appUsageEvent);
-          results.add(appUsageEvent);
           listener.appOpened(appUsageEvent, isAppOfInterestForOpening(appUsageEvent));
         }
 
       }
     }
-    return results;
   }
 
 
